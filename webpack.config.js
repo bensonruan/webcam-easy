b0b2603261d75bbc1b16ad49c12367ad5acd60e4
Ronald M. Kasendwa <kasendwa@awamo.com>
2020-06-07 17:03:01 +0300

    Fixes webpack build issues

--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,32 @@
 var webpack = require('webpack');
 var path = require('path');
-var pkg = require('./cropit.jquery.json');
 var paths = {
 	src: path.join(__dirname, 'src'),
 	dist: path.join(__dirname, 'dist'),
 };
 module.exports = {
-	entry: paths.src + '/plugin.js',
+	entry: paths.src + '/webcam-easy.js',
 	output: {
 		path: paths.dist,
-		filename: 'jquery.cropit.js',
-		library: 'cropit',
-		libraryTarget: 'umd',
+		filename: 'webcam-easy.js',
+		library: 'webcam-easy',
+		libraryTarget: 'umd'
 	},
 	module: {
 		rules: [{
 			test: /\.js$/,
 			exclude: /node_modules/,
-			loader: 'babel-loader?blacklist[]=strict',
+			use: {
+				loader: 'babel-loader',
+				options: {
+					presets: ["@babel/preset-env"],
+					plugins: [
+						"@babel/plugin-syntax-class-properties",
+						"@babel/plugin-proposal-class-properties",
+						"@babel/plugin-proposal-object-rest-spread"
+					]
+				}
+			}
 		}]
-	},
-	externals: {
-		jquery: {
-			root: 'jQuery',
-			commonjs: 'jquery',
-			commonjs2: 'jquery',
-			amd: 'jquery',
-		},
-	},
-	plugins: [
-		new webpack.BannerPlugin(pkg.name + ' - v' + pkg.version +
-			' <' + pkg.homepage + '>'),
-	],
+	}
 };
