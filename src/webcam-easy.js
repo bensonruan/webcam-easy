4fb0c0ba8537715f0c8a4f6bfb6327113854bc96
Sergej Görzen <sergej-goerzen@web.de>
2023-12-28 07:29:53 +0100

    Update webcam-easy.js
    fixed typo

--- src/webcam-easy.js
+++ src/webcam-easy.js
@@ -77,7 +77,7 @@ export default class Webcam {
 
     /* Change Facing mode and selected camera */ 
     flip(){
-      this._facingMode = (this._facingMode == 'user')? 'enviroment': 'user';
+      this._facingMode = (this._facingMode == 'user')? 'environment': 'user';
       this._webcamElement.style.transform = "";
       this.selectCamera();  
     }
